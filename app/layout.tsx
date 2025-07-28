d9b966e5d07f75b3361a266550b20ffe43658ccd
Nitin0raon <ornitin89@gmail.com>
2025-07-28 11:23:54 +0530

    navbar dark mode done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-
-import { Geist, Geist_Mono } from "next/font/google";
+import ClerkThemeProvider from "@/components/ClerkThemeProveder";
 import "./globals.css";
+import { ThemeProvider } from "@/contexts/themeContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,15 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ThemeProvider>
+    <ClerkThemeProvider>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         {children}
       </body>
     </html>
-    </ClerkProvider>
+    </ClerkThemeProvider>
+    </ThemeProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-red-400">hello</div>
+    <div></div>
     
   );
 }
