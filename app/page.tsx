ca05437f88e9cf4f5b7d9b5010fc75d903d8fb74
Nitin0raon <ornitin89@gmail.com>
2025-08-02 18:30:01 +0530

    complete frontend excluding loggedin dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "@/components/Guest";
+export default async function Home() {
+  const user =await currentUser();
 
-export default function Home() {
+  if(!user){
+    return<Guest/>
+  }
   return (
     <div></div>
     
